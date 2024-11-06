7b28742e7edd4e5f7a8798c924abf8ed5af4618b
Volodymyr Marukhno <vlama4me@gmail.com>
2024-11-06 14:08:43 +0200

    server

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,7 +31,7 @@ app.use('/search', searchRoutes);
 app.use('/users', userRoutes);
 app.use('/teams', teamRoutes);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
